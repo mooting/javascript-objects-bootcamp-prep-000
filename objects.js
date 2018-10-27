deb9234e7129f4618ef2532e2a390fac2ebb871f
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 18:17:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist = { 'Johhny Cash': 'I Walk the Line' }
 
 function updatePlaylist ( playlist, artistName, songTitle ) { playlist[artistName] = songTitle }
 
-function removeFromPlaylist ( playlist, artistName ) { delete playlist.artistName }
+function removeFromPlaylist ( playlist, artistName ) { delete playlist[artistName] }
